6bfd18f3e0b87aa99b23c1532e3308c2e9177250
Your Name <you@example.com>
2023-12-20 23:56:43 +0600

    error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,18 @@
-
+import { Link } from "react-router-dom";
+import { MdKeyboardBackspace } from "react-icons/md";
 
 
 const ErrorPage = () => {
     return (
-        <div>
+        <div className="text-center text-5xl mt-10">
+            <h2 className="text-warning">Oops!!!</h2>
 
+            <span className="flex justify-center mt-3 items-center">
+                <Link to="/"> <MdKeyboardBackspace className="text-lime-500"></MdKeyboardBackspace></Link>
+                <Link className="text-lime-500" to="/">Back to home</Link>
+            </span>
         </div>
-    );
+    )
 };
 
 export default ErrorPage;
--- src/pages/Shared/Login/Login.jsx
+++ src/pages/Shared/Login/Login.jsx
@@ -1,11 +1,18 @@
 import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 
-
+const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const repeat = form.repeat.value;
+    console.log(email, password, repeat)
+}
 
 const Login = () => {
     return (
         <div className="flex justify-center mt-10">
-            <form className="flex max-w-md flex-col gap-4 w-full">
+            <form onSubmit={handleLogin} className="flex max-w-md flex-col gap-4 w-full">
                 <div>
                     <div className="mb-2 block">
                         <Label htmlFor="email1" value="Your email" />
@@ -18,12 +25,18 @@ const Login = () => {
                     </div>
                     <TextInput placeholder="password" id="password1" name="password" type="password" required />
                 </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="repeat-password" value="Repeat password" />
+                    </div>
+                    <TextInput name="repeat" id="repeat-password" type="password" required shadow />
+                </div>
                 <div className="flex items-center gap-2">
                     <Checkbox id="remember" />
                     <Label htmlFor="remember">Remember me</Label>
                 </div>
                 <input className="bg-red-700 rounded py-2" type="submit" value="Submit" />
-                <Button color="success">Submit</Button>
+                <Button color="success" type="submit">Submit</Button>
             </form>
         </div>
     );
